d9ec82b6956f7ee9f712f0c76cd5e87a8ee1760a
Onur Akşar <onur.aksar@akinon.com>
2022-12-15 23:42:38 +0300

    counter done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, {useState, useReducer} from 'react'
 
-    </div>
+
+// Global- non-changing variables:
+const ACTIONS = {
+    INCREMENT: 'increment',
+    DECREMENT: 'decrement',
+    CLEAR: 'clear'
+}
+
+function reducer(state, action) {
+  console.log("state", state)
+  console.log("action", action)
+  switch(action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 }
+    case ACTIONS.DECREMENT :
+      return { count: state.count -1 }
+    case ACTIONS.CLEAR :
+      return { count: 0 }
+    default:
+      return state
+  }
+}
+
+
+function App() {
+  
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+  // Similar to useState:
+  // const [count, setCount] = useState(0)
+
+  function increment () {
+    dispatch({type: 'increment'})
+  }
+
+  function decrement () {
+    state.count > 0 && dispatch({type: 'decrement'})
+  }
+
+  function clear () {
+    dispatch({type: 'clear'})
+  }
+
+
+  return (
+    <>
+    <button onClick={decrement}>-</button>
+    <span>{state.count}</span>
+    <button onClick={increment}>+</button>
+    <button onClick={clear}>CLEAR</button>
+    </>
   );
 }
 
